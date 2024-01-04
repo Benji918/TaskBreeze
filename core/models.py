6d11e6be2d1309b519e48c63e1680643a2b7853e
USER <kodiugos@gmail.com>
2024-01-04 04:52:51 +0000

    Created user model manager and user model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from .manager import CustomUserManager
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext as _
+
 
 # Create your models here.
+class CustomUser(AbstractUser):
+    first_name = models.CharField(_('first_name'), unique=True, max_length=50, blank=False, null=False)
+    last_name = models.CharField(_('last_name'), unique=True, max_length=50, blank=False, null=False)
+    email = models.EmailField(_('email address'), unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ('username',)
+
+    def __str__(self):
+        return self.email
