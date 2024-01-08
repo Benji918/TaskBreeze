4f0149dd270aa8283891b13793f642745838c3f8
Benjamin <kodiugos@gmail.com>
2024-01-08 00:54:58 +0000

    altered custom user model

--- core/models.py
+++ core/models.py
@@ -2,13 +2,24 @@ from django.db import models
 from .manager import CustomUserManager
 from django.contrib.auth.models import AbstractUser
 from django.utils.translation import gettext as _
+import uuid
 
 
 # Create your models here.
 class CustomUser(AbstractUser):
-    first_name = models.CharField(_('first_name'), unique=True, max_length=50, blank=False, null=False)
-    last_name = models.CharField(_('last_name'), unique=True, max_length=50, blank=False, null=False)
+    ROLE_CHOICES = (
+        ('user', 'user'),
+        ('admin', 'admin'),
+    )
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False,
+        unique=True
+    )
+    username = models.CharField(_('first_name'), unique=True, max_length=50, blank=False, null=False)
     email = models.EmailField(_('email address'), unique=True)
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='user')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
