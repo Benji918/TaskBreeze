45101b8c78ccb34ce6699f13077c35abd1ef245c
Benjamin <kodiugos@gmail.com>
2024-01-08 01:19:03 +0000

    added task model

--- core/models.py
+++ core/models.py
@@ -3,6 +3,7 @@ from .manager import CustomUserManager
 from django.contrib.auth.models import AbstractUser
 from django.utils.translation import gettext as _
 import uuid
+from django.conf import settings
 
 
 # Create your models here.
@@ -30,3 +31,33 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.email
+
+
+class Task(models.Model):
+    STATUS_CHOICES = (
+        ('open', 'open'),
+        ('in progress', 'in progress'),
+        ('completed', 'completed'),
+    )
+    PRIORITY_CHOICES = (
+        ('high', 'high'),
+        ('medium', 'medium'),
+        ('low', 'low')
+    )
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False,
+        unique=True
+    )
+    title = models.CharField(unique=True, max_length=50, blank=False, null=False)
+    description = models.CharField(unique=True, max_length=50, blank=False, null=False)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='open')
+    priority = models.CharField(max_length=10, choices=PRIORITY_CHOICES, default='low')
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    due_date = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
