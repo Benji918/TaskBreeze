769b95a4ca7bcf8e38c0cd12960fe07817cffdca
Benjamin <kodiugos@gmail.com>
2024-01-16 05:26:26 -0500

    file edits

--- taskbreeze/urls.py
+++ taskbreeze/urls.py
@@ -35,7 +35,7 @@ urlpatterns = [
     # application urls
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
-    path('tasks/', include('tasks.urls')),
+    # path('tasks/', include('tasks.urls')),
 ]
 
 if settings.DEBUG:
